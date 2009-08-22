929b168406c0891d13a489b8def7a496ce3cbfdb
jaderd <jaderd@f9f50b74-8f3f-11de-8988-25e8a898b690>
2009-08-22 17:26:31 +0000

    Created project Created ValuesClass
    Added Properties equality check

---
+++ IEquatableReference/trunk/IEquatableReference/ValuesClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IEquatableReference
+{
+    public class ValuesClass : IEquatable<ValuesClass>
+    {
+        public int First { get; set; }
+        public double Second { get; set; }
+
+        #region IEquatable<ValuesClass> Members
+
+        public bool Equals(ValuesClass other)
+        {
+            return this.First.Equals(other.First)
+                && this.Second.Equals(other.Second);
+        }
+
+        #endregion
+    }
+}
