4f39d4d024316cfdf900cc89abdd2fd8a18aed0f
jaderd <jaderd@f9f50b74-8f3f-11de-8988-25e8a898b690>
2009-08-22 18:37:10 +0000

    Corrected explanation

--- IEquatableReference/trunk/IEquatableReference/ValuesClass.cs
+++ IEquatableReference/trunk/IEquatableReference/ValuesClass.cs
@@ -29,6 +29,7 @@ namespace IEquatableReference
         public override int GetHashCode()
         {
             //this.First doesn't need .GetHashCode()
+            //bitwise XOR operator is chosen to achieve uniform distribution and to avoid arithmetic overflow
             return this.First ^ this.Second.GetHashCode();
         }
 
